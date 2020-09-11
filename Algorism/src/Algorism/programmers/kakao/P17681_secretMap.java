042f18e589321e58fad84d48733277be0085801b
sujl950620 <tlaf2000@naver.com>
2020-09-11 17:33:53 +0900

    update

--- Algorism/src/Algorism/programmers/kakao/P17681_secretMap.java
+++ Algorism/src/Algorism/programmers/kakao/P17681_secretMap.java
@@ -4,17 +4,18 @@ import java.util.Arrays;
 
 public class P17681_secretMap {
 
-	public static void main(string[] args) {
+	public static void main(String[] args) {
 		int[] arr1 ={46, 33, 33, 22, 31, 50};
 		int[] arr2 = {27, 56, 19, 14, 14, 10};
-		system.out.println(arrays.tostring(solution(6, arr1, arr2)));
+		System.out.println(Arrays.toString(solution(6, arr1, arr2)));
 	}
 
 
-	public static string[] solution(int n, int[] arr1, int[] arr2) {
-        string[] answer = new string[n];
+	public static String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
         for(int i = 0 ; i<n; i++) {
-        	answer[i]= string.format("%"+n+"s",(integer.tobinarystring(arr1[i] | arr2[i])).replaceall("1", "#").replaceall("0"," "));
+        	answer[i]= String.format("%"+n+"s",
+					(Integer.toBinaryString(arr1[i] | arr2[i])).replaceAll("1", "#").replaceAll("0"," "));
         }
         return answer;
     }
