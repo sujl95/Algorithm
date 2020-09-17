f70ef1b19dc25a4821b8885c736e251bd4672d68
sujl95 <sujl95@naver.com>
2020-09-18 01:47:47 +0900

    test

---
+++ Algorism/src/Algorism/test.java
@@ -0,0 +1,19 @@
+package Algorism;
+
+import java.util.Arrays;
+
+public class test {
+    public static void main(String[] args) {
+        test test = new test();
+
+        method(new int[]{1,2,3,4,5});
+        method(1,2,3,4,5);
+    }
+
+
+    public static void method(int...numbers){
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+}
