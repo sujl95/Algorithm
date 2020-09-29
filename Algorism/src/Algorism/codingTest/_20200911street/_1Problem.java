9e03b2bda224a4125542cf4d3db959c1624af099
sujl95 <sujl95@naver.com>
2020-09-29 12:03:11 +0900

    11street

---
+++ Algorism/src/Algorism/codingTest/_20200911street/_1Problem.java
@@ -0,0 +1,29 @@
+package Algorism.codingTest._20200911street;
+
+public class _1Problem {
+    public static void main(String[] args) {
+        System.out.println(solution("aabab"));
+        System.out.println(solution("dog"));
+    }
+
+    public static int solution(String S) {
+        if(S.contains("aaaa")) return -1;
+        if(S.length() == 2 && S.contains("aa")) return 0;
+        int aCnt = 0;
+        int aPCnt = 0;
+        String str = "";
+        for(int i =0 ; i< S.length();i++){
+            char c = S.charAt(i);
+            if(c == 'a'){
+                aCnt++;
+            } else {
+                str += "a"+c+"a";
+                aPCnt++;
+            }
+        }
+
+        str = "a"+str+"a";
+        aPCnt++;
+        return (aPCnt*2)-aCnt;
+    }
+}
