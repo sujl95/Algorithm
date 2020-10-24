d587e8bfb620e44cf49e421dba723b7547942959
sujl95 <sujl95@naver.com>
2020-10-24 15:50:09 +0900

    NHN

---
+++ Algorism/src/Algorism/codingTest/_202010NHN/_01.java
@@ -0,0 +1,57 @@
+package Algorism.codingTest._202010NHN;
+
+import java.util.*;
+
+public class _01 {
+
+    private static void solution(int numOfAllPlayers, int numOfQuickPlayers, char[] namesOfQuickPlayers, int numOfGames, int[] numOfMovesPerGame){
+        // TODO: 이곳에 코드를 작성하세요. 추가로 필요한 함수와 전역변수를 선언해서 사용하셔도 됩니다.
+        System.out.println("numOfAllPlayers="+numOfAllPlayers+", numOfQuickPlayers="+numOfQuickPlayers+", namesOfQuickPlayers="+ Arrays.toString(namesOfQuickPlayers)+"numOfGames="+numOfGames+", numOfMovesPerGame="+Arrays.toString(numOfMovesPerGame));
+        HashMap<String, Integer> map = new HashMap<>();
+        Queue<HashMap<String, Integer>> q = new LinkedList<HashMap<String, Integer>>();
+        char a = 'A';
+        for(int i = 0 ; i<numOfAllPlayers; i++){
+            map.put((char)(a+i)+"",0);
+            q.add(map);
+
+        }
+        System.out.println(q);
+    }
+
+    private static class InputData {
+        int numOfAllPlayers;
+        int numOfQuickPlayers;
+        char[] namesOfQuickPlayers;
+        int numOfGames;
+        int[] numOfMovesPerGame;
+    }
+
+    private static InputData processStdin() {
+        InputData inputData = new InputData();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            inputData.numOfAllPlayers = Integer.parseInt(scanner.nextLine().replaceAll("\\s+", ""));
+
+            inputData.numOfQuickPlayers = Integer.parseInt(scanner.nextLine().replaceAll("\\s+", ""));
+            inputData.namesOfQuickPlayers = new char[inputData.numOfQuickPlayers];
+            System.arraycopy(scanner.nextLine().trim().replaceAll("\\s+", "").toCharArray(), 0, inputData.namesOfQuickPlayers, 0, inputData.numOfQuickPlayers);
+
+            inputData.numOfGames = Integer.parseInt(scanner.nextLine().replaceAll("\\s+", ""));
+            inputData.numOfMovesPerGame = new int[inputData.numOfGames];
+            String[] buf = scanner.nextLine().trim().replaceAll("\\s+", " ").split(" ");
+            for(int i = 0; i < inputData.numOfGames ; i++){
+                inputData.numOfMovesPerGame[i] = Integer.parseInt(buf[i]);
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+
+        return inputData;
+    }
+
+    public static void main(String[] args) throws Exception {
+        InputData inputData = processStdin();
+
+        solution(inputData.numOfAllPlayers, inputData.numOfQuickPlayers, inputData.namesOfQuickPlayers, inputData.numOfGames, inputData.numOfMovesPerGame);
+    }
+}
