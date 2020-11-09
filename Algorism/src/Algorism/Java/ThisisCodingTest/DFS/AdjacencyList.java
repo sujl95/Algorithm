67825fa2c0aed1a1a72483c6dc88551449701011
sujl95 <sujl95@naver.com>
2020-11-10 02:57:09 +0900

    AdjacencyMatrix , AdjacencyList, DFS

---
+++ Algorism/src/Algorism/Java/ThisisCodingTest/DFS/AdjacencyList.java
@@ -0,0 +1,47 @@
+package Algorism.Java.ThisisCodingTest.DFS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    private int index;
+    private List<Node> connectedNodes = new ArrayList<>();
+
+    public Node(int index) {
+        this.index = index;
+    }
+
+    public void connectNode(Node node) {
+        connectedNodes.add(node);
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "index=" + index +
+                ", connectedNodes=" + connectedNodes +
+                '}';
+    }
+}
+
+
+public class AdjacencyList {
+
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+
+    public static void main(String[] args) {
+//        for (int i = 0; i < 3; i++) {
+//            graph.add(new ArrayList<Node>());
+//        }
+//        graph.get(0).add(new Node(1, 7));
+//        graph.get(0).add(new Node(5, 5));
+//        graph.get(1).add(new Node(0,7));
+//        graph.get(2).add(new Node(0, 5));
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < graph.get(i).size(); j++) {
+//                graph.get(i).get(j).show();
+//            }
+//            System.out.println();
+//        }
+    }
+}
---
+++ Algorism/src/Algorism/Java/ThisisCodingTest/DFS/DFSSample.java
@@ -0,0 +1,39 @@
+package Algorism.Java.ThisisCodingTest.DFS;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DFSSample {
+//    private static List<Node> edges = new ArrayList<>();
+
+    private static boolean[] visited ;
+
+    public static void main(String[] args) {
+        int n = 8;
+        visited = new boolean[n];
+
+        int[][] edges = new int[][] {
+            {0,1,1,0,0,0,0,1},
+            {1,0,0,0,0,0,1,0},
+            {1,0,0,1,1,0,0,0},
+            {0,0,1,0,1,0,0,0},
+            {0,0,1,1,0,0,0,0},
+            {0,0,0,0,0,0,1,0},
+            {0,1,0,0,0,1,0,1},
+            {1,0,0,0,0,0,1,0}
+        };
+
+        dfs(edges,0);
+    }
+
+    public static void dfs(int[][] edges, int i) {
+        System.out.print(i+1 + " ");
+        visited[i] = true;
+        for (int j = 0; j < edges[i].length; j++) {
+            if (edges[i][j] == 1 && visited[j] == false) {
+                dfs(edges,j);
+            }
+        }
+    }
+}
