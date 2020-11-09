233813f0b09428fbf54b04d9baf8ae302fd19ce3
sujl95 <sujl95@naver.com>
2020-11-10 03:01:38 +0900

    AdjacencyList BackUp

--- Algorism/src/Algorism/Java/ThisisCodingTest/DFS/AdjacencyList.java
+++ Algorism/src/Algorism/Java/ThisisCodingTest/DFS/AdjacencyList.java
@@ -3,6 +3,20 @@ package Algorism.Java.ThisisCodingTest.DFS;
 import java.util.ArrayList;
 import java.util.List;
 
+//class Node {
+//    private int index;
+//    private int distance;
+//
+//    public Node(int index, int distance) {
+//        this.index = index;
+//        this.distance = distance;
+//    }
+//
+//    public void show() {
+//        System.out.print("(" + this.index + "," + this.distance + ")");
+//    }
+//}
+
 class Node {
     private int index;
     private List<Node> connectedNodes = new ArrayList<>();
