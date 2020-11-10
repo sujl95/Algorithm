9724a3f7a6e5016302d305d6ac15f85548119d03
sujl95 <sujl95@naver.com>
2020-11-11 02:27:39 +0900

    BFS

--- Algorism/src/Algorism/Java/ThisisCodingTest/BFS/_1BFSSample.java
+++ Algorism/src/Algorism/Java/ThisisCodingTest/BFS/_1BFSSample.java
@@ -26,26 +26,19 @@ public class _1BFSSample {
         Queue<Integer> q = new LinkedList();
         q.offer(i);
         visited[i] = true;
-        System.out.print(i + " ");
-        System.out.println(Arrays.toString(visited));
-        System.out.println(q);
         //2. 큐에서 노드를 꺼내 해당 노드의 인접 노드 중에서
         // 방문하지 않은 노드를 모두 큐에 삽입하고 방문 처리를 한다
         //3. 2번 과정을 더 이상 수행할 수 없을 때까지 반복한다.
         while (!(q.isEmpty())) {
-            int x = q.poll();
-            System.out.println(x);
-            System.out.print(x+ " ");
-            for(int j = 0; j< edges[x].length; j++){
-                int y = edges[x][j];
-                if (!visited[y]){
-                    q.offer(y);
-                    visited[y] = true;
+            i = q.poll();
+            System.out.print(i+1+ " ");
+            for(int j = 0; j< edges[i].length; j++){
+                int y = j;
+                if (edges[i][j] == 1 && !visited[y]){
+                    visited[j] = true;
+                    q.offer(j);
                 }
             }
         }
-
-        System.out.println(Arrays.toString(visited));
-
     }
 }
