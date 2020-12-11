7837265bf176000240ad27bd9c98518e6acdaffb
sujl95 <sujl95@naver.com>
2020-12-12 00:10:16 +0900

    프로그래머스 H-Index , 백준 괄호 9012

---
+++ Algorism/src/Algorism/Java/backjun/Stack/J9012.java
@@ -0,0 +1,30 @@
+package Algorism.Java.backjun.Stack;
+
+import java.util.*;
+
+public class J9012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n --> 0) {
+            String s = sc.next();
+            Stack<Character> stk = new Stack<>();
+            boolean ck = true;
+            char[] arr = s.toCharArray();
+            for(char c : arr) {
+                if (c == '(')
+                    stk.push(c);
+                else {
+                    if(!stk.isEmpty())
+                        stk.pop();
+                    else {
+                        ck = false;
+                        break;
+                    }
+                }
+            }
+            if (!stk.isEmpty()) ck = false;
+            System.out.println(ck ? "YES" : "NO");
+        }
+    }
+}
