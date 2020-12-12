b71fbd3257b8ec87b05249438381c1a787c94cdd
sujl95 <sujl95@naver.com>
2020-12-13 00:45:22 +0900

    selectSort

--- Algorism/src/Algorism/Python/ThisisCodingTest/sort/selectSort.py
+++ Algorism/src/Algorism/Python/ThisisCodingTest/sort/selectSort.py
@@ -3,7 +3,7 @@ array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 for i in range(len(array)):
     minIndex = i # 가장 작은 원소의 인덱스
     for j in range(i + 1, len(array)):
-        if array(minIndex) > array[j]:
+        if array[minIndex] > array[j]:
             minIndex = j
     array[i], array[minIndex] = array[minIndex], array[i] # 스와프
 
