15bdf1474210ff55cb6f148b66cf3ebcfd2609c7
sujl95 <sujl95@naver.com>
2020-12-13 01:05:58 +0900

    P70128 내적 https://programmers.co.kr/learn/courses/30/lessons/70128

---
+++ Algorism/src/Algorism/Java/programmers/P70128.java
@@ -0,0 +1,19 @@
+package Algorism.Java.programmers;
+
+import java.util.stream.IntStream;
+
+public class P70128 {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2};
+
+        solution(a,b);
+    }
+
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0 ; i < a.length; i++)
+            answer += a[i]* b[i];
+        return answer;
+    }
+}
