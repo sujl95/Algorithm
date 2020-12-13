bfa38c4947e330f505ca6d443a6ea18dd0200c2e
sujl95 <sujl95@naver.com>
2020-12-14 04:04:48 +0900

    같은 숫자는 싫어 https://programmers.co.kr/learn/courses/30/lessons/12906

--- Algorism/src/Algorism/Python/programmers/P12906.py
+++ Algorism/src/Algorism/Python/programmers/P12906.py
@@ -1,10 +1,9 @@
 def solution(arr):
     answer = []
     answer.append(arr[0])
-    if len(arr) != 0:
-        for i in range(1, len(arr)):
-            if arr[i] != arr[i-1]:
-                answer.append(arr[i])
+    for i in range(1, len(arr)):
+        if arr[i] != arr[i-1]:
+            answer.append(arr[i])
     return answer
 
 
