7c53a5c93fa03254758745c3e7f79838cf7b3428
sujl95 <sujl95@naver.com>
2020-12-15 00:00:47 +0900

    체육복 생성 https://programmers.co.kr/learn/courses/30/lessons/42862

---
+++ Algorism/src/Algorism/Java/programmers/greedy/P42862.java
@@ -0,0 +1,15 @@
+package Algorism.Java.programmers.greedy;
+
+public class P42862 {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+
+        solution(n, lost, reserve);
+    }
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        return answer;
+    }
+}
