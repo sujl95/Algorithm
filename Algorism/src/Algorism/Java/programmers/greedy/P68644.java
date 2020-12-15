4df7b1ede3ee7a448848da483441951f30b6f42e
sujl95 <sujl95@naver.com>
2020-12-15 22:30:41 +0900

    두 개 뽑아서 더하기 https://programmers.co.kr/learn/courses/30/lessons/68644

---
+++ Algorism/src/Algorism/Java/programmers/greedy/P68644.java
@@ -0,0 +1,25 @@
+package Algorism.Java.programmers.greedy;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class P68644 {
+    public static void main(String[] args) {
+
+        System.out.println(solution( 2,1,3,4,1));
+    }
+
+    public static int[] solution(int... numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i == j) {
+                } else {
+                    set.add(numbers[i] + numbers[j]);
+                }
+            }
+        }
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
