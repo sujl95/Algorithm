c17a3650cb1ed68eefae6c52143542b606b02e38
sujl95 <sujl95@naver.com>
2020-12-16 00:31:47 +0900

    del sample

--- Algorism/src/Algorism/test.java
+++ Algorism/src/Algorism/test.java
@@ -4,16 +4,12 @@ import java.util.Arrays;
 
 public class test {
     public static void main(String[] args) {
-        test test = new test();
-
         method(new int[]{1,2,3,4,5});
         method(1,2,3,4,5);
     }
 
 
     public static void method(int...numbers){
-
         System.out.println(Arrays.toString(numbers));
-
     }
 }
