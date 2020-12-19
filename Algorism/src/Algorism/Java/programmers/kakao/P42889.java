4873e5af4f27b7eabf6b4691e12082a9d064af7c
sujl95 <sujl95@naver.com>
2020-12-19 13:48:13 +0900

    2019 카카오 블라인드 실패율 java https://programmers.co.kr/learn/courses/30/lessons/42889

---
+++ Algorism/src/Algorism/Java/programmers/kakao/P42889.java
@@ -0,0 +1,34 @@
+package Algorism.Java.programmers.kakao;
+
+import java.util.*;
+
+public class P42889 {
+
+    public static void main(String[] args) {
+//        System.out.println(solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3}));
+        System.out.println(solution(4, new int[]{4,4,4,4,4}));
+    }
+
+    public static int[] solution(int N, int[] stages) {
+        int people = stages.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Double> res = new HashMap<>();
+        for (int i = 0; i < stages.length; i++) {
+            if (map.get(stages[i]) == null)
+                map.put(stages[i], 1);
+            else map.put(stages[i], map.get(stages[i])+1);
+        }
+        for (int i = 1; i<= N; i++) {
+            if (map.containsKey(i)) {
+                res.put(i,(double)map.get(i)/people);
+                people -= map.get(i);
+            } else res.put(i,0.0);
+        }
+        List<Map.Entry<Integer,Double>> list = new ArrayList<>(res.entrySet());
+        Collections.sort(list, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++)
+            answer[i] = list.get(i).getKey();
+        return answer;
+    }
+}
