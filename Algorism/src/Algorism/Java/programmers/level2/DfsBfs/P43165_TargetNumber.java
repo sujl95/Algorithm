0bbde40733c2f9556bdbc532e037f88d9a88f1f9
sujl95 <sujl95@naver.com>
2021-02-15 22:34:57 +0900

    프로그래머스 타겟넘버 43165 https://programmers.co.kr/learn/courses/30/lessons/43165

---
+++ Algorism/src/Algorism/Java/programmers/level2/DfsBfs/P43165_TargetNumber.java
@@ -0,0 +1,23 @@
+package Algorism.Java.programmers.level2.DfsBfs;
+
+public class P43165_TargetNumber {
+
+	public static void main(String[] args) {
+		int[] numbers = {1, 1, 1, 1, 1};
+		int target = 1;
+		System.out.println("solution() = " + solution(numbers,target));
+	}
+
+	public static int solution(int[] numbers, int target) {
+		return dfs(numbers, target, 0, 0);
+	}
+
+	private static int dfs(int[] numbers, int target, int idx, int number) {
+		if (idx == numbers.length) {
+			return number == target ? 1 : 0;
+		}
+		return dfs(numbers, target, idx + 1, number + numbers[idx])
+				+ dfs(numbers, target, idx + 1, number - numbers[idx]);
+	}
+
+}
