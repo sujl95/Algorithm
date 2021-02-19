cae8c2462262118cf154c00648601e2fb89b1b92
sujl95 <sujl95@naver.com>
2021-02-19 17:18:17 +0900

    프로그래머스 땅따먹기 12913 java
    https://programmers.co.kr/learn/courses/30/lessons/12913

--- Algorithm/src/Algorithm/Java/programmers/level2/practice/P12913.java
+++ Algorithm/src/Algorithm/Java/programmers/level2/practice/P12913.java
@@ -1,17 +1,21 @@
 package Algorithm.Java.programmers.level2.practice;
 
+import java.util.Arrays;
 
 public class P12913 {
 
 	public static void main(String[] args) {
-		int[][] land = new int[][]{{1,2,5,3},{5,6,7,80},{4,3,2,1}};
-
+		int[][] land = new int[][] {{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}};
 		System.out.println(solution(land));
 	}
 
 	public static int solution(int[][] land) {
-		int answer = 0;
-
-		return answer;
+		for (int i = 1; i < land.length; i++) {
+			land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+			land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+			land[i][2] += Math.max(land[i - 1][1], Math.max(land[i - 1][0], land[i - 1][3]));
+			land[i][3] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][0]));
+		}
+		return Arrays.stream(land[land.length-1]).max().getAsInt();
 	}
 }
