3b6f8df2fdd47641481b07b38a0c7cf338acdf1c
sujl95 <sujl95@naver.com>
2021-02-20 13:35:13 +0900

    backjun LoopWhile Update

--- Algorithm/src/Algorithm/Java/backjun/LoopWhile/J10951.java
+++ Algorithm/src/Algorithm/Java/backjun/LoopWhile/J10951.java
@@ -5,7 +5,7 @@ public class J10951 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(sc.hasNextInt()) {
-			System.out.println(sc.nextInt()+sc.nextInt());
+			System.out.println(sc.nextInt() + sc.nextInt());
 		}
 	}
 }
--- Algorithm/src/Algorithm/Java/backjun/LoopWhile/J1110.java
+++ Algorithm/src/Algorithm/Java/backjun/LoopWhile/J1110.java
@@ -6,24 +6,27 @@ public class J1110 {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
 		String nw = str;
-		int ans = -1;
+		int ans ;
 		int cnt = 0;
-		boolean ck= true;
+		boolean ck = true;
 		while(ck) {
 			if ( nw.length() == 1 ) {
-				nw ="0"+nw;
+				nw = "0" + nw;
 			}
-			String a = nw.substring(0,1);
-			String b = nw.substring(1,2);
+			String a = nw.substring(0, 1);
+			String b = nw.substring(1, 2);
 			ans = Integer.parseInt(a) + Integer.parseInt(b);
-			if ( Integer.toString(ans).length() > 1 ) {
-				nw = "" + b+ Integer.toString(ans).substring(1,2);
+			if (Integer.toString(ans).length() > 1) {
+				nw = "" + b + Integer.toString(ans).charAt(1);
+			} else {
+				nw = "" + b + ans;
 			}
-			else nw = ""+ b + ans;
-			if(Integer.parseInt(nw) <10 ) {
+			if (Integer.parseInt(nw) < 10) {
 				nw = Integer.toString(Integer.parseInt(nw));
 			}
-			if(str.equals(nw)) ck = false ;
+			if (str.equals(nw)) {
+				ck = false;
+			}
 			cnt++;
 		}
 		System.out.println(cnt);
