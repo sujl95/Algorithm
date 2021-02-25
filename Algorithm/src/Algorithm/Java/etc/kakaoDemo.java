82708a71e5604ce3a0e078e6955a27a5c93bfbfa
sujl95 <sujl95@naver.com>
2021-02-25 22:10:06 +0900

    toss etc Refactoring

--- Algorithm/src/Algorithm/Java/etc/kakaoDemo.java
+++ Algorithm/src/Algorithm/Java/etc/kakaoDemo.java
@@ -5,41 +5,42 @@ import java.util.*;
 public class kakaoDemo {
 
 	public static void main(String[] args) {
-		
-		int[][] v = {{1,4},{3,4},{3,10}};
-		int[][] v1 = {{3,4},{3,10},{1,4}};
+
+		int[][] v = {{1, 4}, {3, 4}, {3, 10}};
+		int[][] v1 = {{3, 4}, {3, 10}, {1, 4}};
 		int[][] t = {{1, 1}, {2, 2}, {1, 2}};
 		System.out.println(Arrays.toString(solution(v1)));
-		
 	}
 	
 	
     public static int[] solution(int[][] v) {
-        int[] xarr = new int[3];
-        int[] yarr = new int[3];
+        int[] xArr = new int[3];
+        int[] yArr = new int[3];
         int x = 0,y =0;
         for (int i = 0; i< v.length; i++) {
-        	xarr[i] = (v[i][0]);
-        	yarr[i] = (v[i][1]);
+			xArr[i] = (v[i][0]);
+			yArr[i] = (v[i][1]);
         }
-        Arrays.sort(xarr);
-        Arrays.sort(yarr);
-        
-        for(int i =0; i<3; i++) {
-        	int xcnt = 0, ycnt = 0;
-        	for(int j =0; j<3;j++) {
-        		if(xarr[i] == xarr[j]) {
-        			xcnt++;
-        		}
-        		if(yarr[i] == yarr[j]) {
-        			ycnt++;
-        		}
-        	}
-        	if(xcnt == 1) x = xarr[i];
-        	if(ycnt == 1) y = yarr[i];
-        }
-        
-        int[] answer = {x,y};
-        return answer;
+		Arrays.sort(xArr);
+		Arrays.sort(yArr);
+		for (int i = 0; i < 3; i++) {
+			int xCnt = 0;
+			int yCnt = 0;
+			for (int j = 0; j < 3; j++) {
+				if (xArr[i] == xArr[j]) {
+					xCnt++;
+				}
+				if (yArr[i] == yArr[j]) {
+					yCnt++;
+				}
+			}
+			if (xCnt == 1) {
+				x = xArr[i];
+			}
+			if (yCnt == 1) {
+				y = yArr[i];
+			}
+		}
+		return new int[] {x, y};
     }
 }
--- Algorithm/src/Algorithm/Java/etc/test.java
+++ Algorithm/src/Algorithm/Java/etc/test.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class test {
 
 	public static void main(String[] args) {
-		int[] arr = {5,1,2,4,9,0,8,3,6,7};
+		int[] arr = {5, 1, 2, 4, 9, 0, 8, 3, 6, 7};
 		int min;
 		int temp;
-		for(int i=0;i<arr.length-1;i++) {
+		for (int i = 0; i < arr.length - 1; i++) {
 			min = i;
-			for(int j=i+1;j<arr.length;j++) {
-				if(arr[min] > arr[j]) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[min] > arr[j]) {
 					min = j;
 				}
 			}
@@ -19,7 +19,6 @@ public class test {
 			arr[i] = arr[min];
 			arr[min] = temp;
 		}
-		
 		System.out.println(Arrays.toString(arr));
 	}
 
