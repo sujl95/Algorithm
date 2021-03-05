ca543b56f2e7fa7a99468a42bbcf8cf6fceb8215
sujl95 <sujl95@naver.com>
2021-03-06 00:17:46 +0900

    Programmers python

--- Algorithm/src/Algorithm/Python/programmers/P12906.py
+++ Algorithm/src/Algorithm/Python/programmers/P12906.py
@@ -1,6 +1,5 @@
 def solution(arr):
-    answer = []
-    answer.append(arr[0])
+    answer = [arr[0]]
     for i in range(1, len(arr)):
         if arr[i] != arr[i-1]:
             answer.append(arr[i])
