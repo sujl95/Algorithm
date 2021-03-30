18be00107a34d7e23c83d50c02e7eeda431338c2
sujl95 <sujl95@naver.com>
2021-03-30 16:50:02 +0900

    백준 1697 숨바꼭질 https://www.acmicpc.net/problem/1697

---
+++ Algorithm/src/Algorithm/Java/backjun/DFS/J1697.java
@@ -0,0 +1,51 @@
+package Algorithm.Java.backjun.DFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class J1697 {
+	static int n;
+	static int k;
+	static int[] visited = new int[100001];
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		k = sc.nextInt();
+
+		if (n == k) {
+			System.out.println(0);
+			return;
+		}
+		bfs(n);
+
+	}
+
+	private static void bfs(int n) {
+		Queue<Integer> q = new LinkedList<>();
+		visited[n] = 1;
+		q.offer(n);
+		while (!q.isEmpty()) {
+			int value = q.poll();
+			for (int i = 0; i < 3; i++) {
+				int nextValue;
+				if (i == 0) {
+					nextValue = value + 1;
+				} else if (i == 1) {
+					nextValue = value - 1;
+				} else {
+					nextValue = value * 2;
+				}
+				if (nextValue == k) {
+					System.out.println(visited[value]);
+					return;
+				}
+				if (0 <= nextValue && nextValue < visited.length && visited[nextValue] == 0) {
+					q.offer(nextValue);
+					visited[nextValue] = visited[value] + 1;
+				}
+			}
+		}
+	}
+}
