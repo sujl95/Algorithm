e118b35967d3d6c925994a45e9bb21b42959d946
sujl95 <sujl95@naver.com>
2021-05-28 14:13:15 +0900

    큰 수 더하기 A + B(B5) , DFS와 BFS(S2)

---
+++ Algorithm/src/Algorithm/Java/backjun/DFS/J1260_New2_S2.java
@@ -0,0 +1,89 @@
+package Algorithm.Java.backjun.DFS;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class J1260_New2_S2 {
+
+	static List<Integer>[] arr;
+	static boolean[] visited;
+	static int n ;
+	static int m ;
+	static int start;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 받아
+		n = sc.nextInt();
+		m = sc.nextInt();
+		start = sc.nextInt();
+
+		arr = new ArrayList[n + 1];
+		for (int i = 0; i <= n; i++) {
+			arr[i] = new ArrayList<>();
+		}
+
+		// 넣을게
+		for (int i = 0; i < m; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			arr[x].add(y);
+			arr[y].add(x);
+		}
+
+		for (List<Integer> integers : arr) {
+			Collections.sort(integers);
+		}
+
+		visited = new boolean[1_001];
+		dfs(start);
+		System.out.println();
+		visited = new boolean[1_001];
+		bfs();
+
+	}
+
+	public static void dfs(int start) {
+		visited[start] = true;
+		System.out.print(start + " ");
+		for (int i = 0; i < arr[start].size(); i++) {
+			int now = arr[start].get(i);
+			if (!visited[now]) {
+				visited[now] = true;
+				dfs(now);
+			}
+		}
+	}
+
+	// bfs Queue
+	public static void bfs() {
+		// Queue 선언해주고
+		Queue<Integer> q = new ArrayDeque<>();
+		// 첫 번째 넣고
+		q.offer(start);
+		// 체킹해주고
+		visited[start] = true;
+
+		//q에 비어있지 않아야하니까?
+		while(!q.isEmpty()) {
+			// 담겨있는거 빼고
+			int now = q.poll();
+			System.out.print(now + " ");
+
+			// now 와 연결되어있는 노드를 찾아야한다. 이전에 탐색 안한 노드 찾기
+			for (int i = 0; i < arr[now].size(); i++) {
+				int next = arr[now].get(i);
+				if (!visited[next]) {
+					visited[next] = true;
+					q.offer(next);
+				}
+			}
+		}
+	}
+}
---
+++ Algorithm/src/Algorithm/Java/backjun/Math1/J10757_B.java
@@ -0,0 +1,14 @@
+package Algorithm.Java.backjun.Math1;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class J10757_B {
+
+	public static void main(String[] args) {
+		String[] arr = new Scanner(System.in).nextLine().split(" ");
+		BigInteger a = new BigInteger(arr[0]);
+		BigInteger b = new BigInteger(arr[1]);
+		System.out.println(a.add(b));
+	}
+}
