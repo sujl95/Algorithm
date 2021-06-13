991504dbf36c63b6b65cb2dd6506b743dd4904a9
sujl95 <sujl95@naver.com>
2021-06-13 22:22:49 +0900

    J11720 B2 Refactor

--- Algorithm/src/Algorithm/Java/backjun/String/J11720.java
+++ Algorithm/src/Algorithm/Java/backjun/String/J11720.java
@@ -1,18 +1,14 @@
 package Algorithm.Java.backjun.string;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Stream;
 public class J11720 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int sum = 0;
-		String str1 = sc.next();
-		int[] digits = Stream.of(String.valueOf(str1).split("")).mapToInt(Integer::parseInt).toArray();
-		for (int digit : digits) {
-			sum += digit;
-		}
-		System.out.println(sum);
+		sc.nextInt();
+		System.out.println(Arrays.stream(Stream.of(String.valueOf(sc.next()).split("")).mapToInt(Integer::parseInt).toArray()).sum());
 	}
 
 }
