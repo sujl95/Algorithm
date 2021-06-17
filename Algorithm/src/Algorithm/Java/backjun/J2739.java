208cf58712ed63ff6617da2aec65739c5aa070e2
sujl95 <sujl95@naver.com>
2021-06-17 23:24:01 +0900

    1157 distinct 2739 Refactor

--- Algorithm/src/Algorithm/Java/backjun/J2739.java
+++ Algorithm/src/Algorithm/Java/backjun/J2739.java
@@ -1,15 +1,13 @@
 package Algorithm.Java.backjun;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class J2739 {
 
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		for (int j = 1; j < 10 ; j++ ) {
-			System.out.println(N + " * " + j + " = " + (N * j));
-		}
+		IntStream.range(1, 10).mapToObj(j -> N + " * " + j + " = " + (N * j)).forEach(System.out::println);
 	}
 }
