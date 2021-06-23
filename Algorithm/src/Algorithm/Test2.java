7b6bfae52d690b93247a6a7b42505ed6e6560001
sujl95 <sujl95@naver.com>
2021-06-23 20:42:01 +0900

    op.gg 대비 예상 코테

--- Algorithm/src/Algorithm/Test2.java
+++ Algorithm/src/Algorithm/Test2.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 public class Test2 {
 
 	public static void main(String[] args) {
-		String[] targets = {"A","C","B","D","G","E"};
-		String[] arr = {"E","C","G","A","B","D"};
+		// String[] today = {"A","C","B","D","G","E"};
+		// String[] yesterday = {"E","C","G","A","B","D"};
+		String[] today = {"A","B","C","D"};
+		String[] yesterday = {"C","D","A","B"};
 		int[] answer = new int[6];
-		for (int i = 0; i < arr.length; i++) {
-			String s = arr[i];
-			for (int j = 0; j < targets.length; j++) {
-				if (targets[j].equals(s)) {
-					answer[i] = j - i;
+		for (int i = 0; i < yesterday.length; i++) {
+			for (int j = 0; j < today.length; j++) {
+				if (today[j].equals(yesterday[i])) {
+					answer[i] = i - j;
 					break;
 				}
 			}
