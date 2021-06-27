733aa168e509caf43735fc0325854753d9ef937b
sujl95 <sujl95@naver.com>
2021-06-27 21:53:08 +0900

    로프 S4 https://www.acmicpc.net/problem/2217

---
+++ Algorithm/src/Algorithm/Java/backjun/Greedy/J2217_S4.java
@@ -0,0 +1,24 @@
+package Algorithm.Java.backjun.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class J2217_S4 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		int max = arr[N - 1];
+		for (int i = 0; i < N; i++) {
+			max = Math.max(max, arr[i] * (N - i));
+		}
+		System.out.println(max);
+	}
+}
