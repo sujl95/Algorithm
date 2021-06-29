6160028c2bc6a3af847161aaf49939f00171bf32
sujl95 <sujl95@naver.com>
2021-06-29 15:18:21 +0900

    셀프 넘버 S5 https://www.acmicpc.net/problem/4673

---
+++ Algorithm/src/Algorithm/Java/backjun/Greedy/J4673_S5.java
@@ -0,0 +1,26 @@
+package Algorithm.Java.backjun.Greedy;
+
+import java.util.stream.IntStream;
+
+public class J4673_S5 {
+
+	public static void main(String[] args) {
+		boolean[] ck = new boolean[10001];
+		IntStream.rangeClosed(1, 10000)
+				.map(J4673_S5::selfNum)
+				.filter(num -> num < 10001)
+				.forEach(num -> ck[num] = true);
+		IntStream.rangeClosed(1, 10000)
+				.filter(num -> !ck[num])
+				.forEach(System.out::println);
+	}
+
+	private static int selfNum(int i) {
+		int sum = i;
+		while(i != 0) {
+			sum += i % 10;
+			i /= 10;
+		}
+		return sum;
+	}
+}
