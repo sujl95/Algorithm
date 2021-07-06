b45f0936b42bf3928824874a30a68bcdafcd4770
sujl95 <sujl95@naver.com>
2021-07-06 19:29:34 +0900

    섬의 개수 S2 https://www.acmicpc.net/problem/4963

---
+++ Algorithm/src/Algorithm/Java/backjun/DFS/J4963_S2.java
@@ -0,0 +1,55 @@
+package Algorithm.Java.backjun.dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class J4963_S2 {
+
+	static int[] dx = {0, 0, -1, 1, 1, -1, -1, 1};
+	static int[] dy = {-1, 1, 0, 0, 1, 1, -1, -1};
+	static int N;
+	static int M;
+	static int[][] map;
+	static boolean[][] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			N = input[0];
+			M = input[1];
+			if (N == 0 && M == 0) {
+				break;
+			}
+			map = new int[M][N];
+			visited = new boolean[M][N];
+			int cnt = 0;
+			for (int i = 0; i < M; i++) {
+				map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			}
+			for (int i = 0; i < M; i++) {
+				for (int j = 0; j < N; j++) {
+					if (!visited[i][j] && map[i][j] == 1) {
+						dfs(i, j);
+						cnt++;
+					}
+				}
+			}
+			System.out.println(cnt);
+		}
+	}
+
+	private static void dfs(int x, int y) {
+		visited[x][y] = true;
+		for (int k = 0; k < 8; k++) {
+			int nextX = x + dx[k];
+			int nextY = y + dy[k];
+			if (0 <= nextX && nextX < M && 0 <= nextY && nextY < N && map[nextX][nextY] == 1
+					&& !visited[nextX][nextY]) {
+				dfs(nextX, nextY);
+			}
+		}
+	}
+}
