ce36d5072ce4d6762ed41c5e4ae0976852e51636
sujl95 <sujl95@naver.com>
2021-07-11 19:11:01 +0900

    좌표 정렬하기 2 https://www.acmicpc.net/problem/11651

---
+++ Algorithm/src/Algorithm/Java/backjun/sort/J11651_S5.java
@@ -0,0 +1,22 @@
+package Algorithm.Java.backjun.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class J11651_S5 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		}
+
+		Arrays.sort(arr, (o1, o2) -> o1[1] == o2[1] ? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1]));
+		Arrays.stream(arr).forEach(ints -> System.out.println(ints[0] + " " + ints[1]));
+	}
+
+}
