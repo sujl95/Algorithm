a421ac9710b851b086c9f5498cbfb2863e0b0ef2
sujl95 <sujl95@naver.com>
2021-07-16 01:58:25 +0900

    이코테 1만들기

---
+++ Algorithm/src/Algorithm/Java/ThisisCodingTest/dynamicprogramming/_1Make.java
@@ -0,0 +1,29 @@
+package Algorithm.Java.ThisisCodingTest.dynamicprogramming;
+
+public class _1Make {
+
+	public static void main(String[] args) {
+
+		int N = 26;
+
+		int[] dp = new int[30001];
+
+
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] + 1;
+
+			if ((i & 2) == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+
+			if ((i & 3) == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+			if ((i & 5) == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 5] + 1);
+			}
+		}
+
+		System.out.println(dp[N]);
+	}
+}
