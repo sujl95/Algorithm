b43cb646bc7237012ec9581240348a9969305bff
sujl95 <sujl95@naver.com>
2021-07-16 16:41:36 +0900

    피보나치 함수 S3 https://www.acmicpc.net/problem/1003

--- Algorithm/src/Algorithm/Java/backjun/dynamic/J1003.java
+++ Algorithm/src/Algorithm/Java/backjun/dynamic/J1003.java
@@ -1,31 +1,35 @@
 package Algorithm.Java.backjun.dynamic;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class J1003 {
-	static int cnt0= 0;
-	static int cnt1= 0;
-	static int x[] = new int[41];
-	static int pibo(int n) {
-		if(n==0) {
-			cnt0++;
-			return 0;
-		} else if(n==1) {
-			cnt1++;
-			return 1;
-		} else {
-			return pibo(n-1)+pibo(n-2);
+	private static Integer[][] dp = new Integer[41][2];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		dp[0][0] = 1;
+		dp[0][1] = 0;
+		dp[1][0] = 0;
+		dp[1][1] = 1;
+
+		for (int i = 0; i < T; i++) {
+			int N = Integer.parseInt(br.readLine());
+			fibo(N);
+			System.out.println(dp[N][0] + " " + dp[N][1]);
 		}
 	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T=sc.nextInt();
-		for (int i = 0; i < T; i++) {
-			int in =sc.nextInt();
-			cnt0=0;cnt1=0;
-			pibo(in);
-			System.out.println(cnt0+" "+cnt1);
+
+	private static Integer[] fibo(int n) {
+		if (dp[n][0] == null || dp[n][1] == null) {
+			dp[n][0] = fibo(n - 1)[0] + fibo(n - 2)[0];
+			dp[n][1] = fibo(n - 1)[1] + fibo(n - 2)[1];
 		}
+		return dp[n];
 	}
 
 }
