b7f09081d9e328f67424d9d7efa2978c42c1d84f
sujl95 <sujl95@naver.com>
2021-07-27 20:34:41 +0900

    2018 써머/윈터 코딩 스킬트리 level2 https://programmers.co.kr/learn/courses/30/lessons/49993

---
+++ Algorithm/src/Algorithm/Java/programmers/level2/summer_winter/P49993_스킬트리.java
@@ -0,0 +1,47 @@
+package Algorithm.Java.programmers.level2.summer_winter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P49993_스킬트리 {
+
+	public static void main(String[] args) {
+		String skill = "CBD";
+		String[] skill_trees = {"BACDE", "CBADF", "AECB", "BDA"};
+		System.out.println("solution() = " + solution(skill, skill_trees));
+	}
+
+
+	public static int solution(String skill, String[] skill_trees) {
+		int answer = 0;
+		for (int i = 0; i < skill_trees.length; i++) {
+			Map<Character, int[]> map = new HashMap<>();
+			for (int j = 0; j < skill.length(); j++) {
+				char c = skill.charAt(j);
+				map.put(c, new int[] {j, 0});
+			}
+			String skillTree = skill_trees[i];
+			if (isSkillTree(map, skillTree)) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+
+	private static boolean isSkillTree(Map<Character, int[]> map, String skillTree) {
+		int idx = 0;
+		for (int j = 0; j < skillTree.length(); j++) {
+			char c = skillTree.charAt(j);
+			int[] value = map.get(c);
+			if (value != null) {
+				if (idx == value[0]) {
+					idx++;
+					continue;
+				}
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
