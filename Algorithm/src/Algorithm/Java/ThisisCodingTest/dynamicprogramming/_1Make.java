7dd9a98bddd759e1202ae9e38d22f1d5f7170462
sujl95 <sujl95@naver.com>
2022-05-07 19:30:52 +0900

    1만들기 New

--- Algorithm/src/Algorithm/Java/ThisisCodingTest/dynamicprogramming/_1Make.java
+++ Algorithm/src/Algorithm/Java/ThisisCodingTest/dynamicprogramming/_1Make.java
@@ -12,14 +12,14 @@ public class _1Make {
 		for (int i = 2; i <= N; i++) {
 			dp[i] = dp[i - 1] + 1;
 
-			if ((i & 2) == 0) {
+			if ((i % 2) == 0) {
 				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
 			}
 
-			if ((i & 3) == 0) {
+			if ((i % 3) == 0) {
 				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
 			}
-			if ((i & 5) == 0) {
+			if ((i % 5) == 0) {
 				dp[i] = Math.min(dp[i], dp[i / 5] + 1);
 			}
 		}
